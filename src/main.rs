75403a517f055b1c49f70bc0c6b9601506edc495
Ishan Jaiswal <jaiswalishan701@gmail.com>
2024-05-26 11:13:34 +0530

    hmm

--- src/main.rs
+++ src/main.rs
@@ -1,5 +1,5 @@
+use grep::Config;
 use std::env;
-use std::fs;
 use std::process;
 fn main() {
     let args: Vec<String> = env::args().collect();
@@ -7,25 +7,11 @@ fn main() {
         eprintln!("Problem parsing arguments: {}", err);
         process::exit(1);
     });
+
     println!("Searching for {}", config.query);
     println!("In file {}", config.file_path);
-}
-fn run(config: Config) -> Result<(), Box<dyn std::error::Error>> {
-    let contents = fs::read_to_string(config.file_path)?;
-    println!("With text:\n{}", contents);
-    Ok(())
-}
-struct Config {
-    query: String,
-    file_path: String,
-}
-impl Config {
-    fn build(args: &[String]) -> Result<Config, &'static str> {
-        if args.len() < 3 {
-            return Err("not enough arguments");
-        }
-        let query = args[1].clone();
-        let file_path = args[2].clone();
-        Ok(Config { query, file_path })
+    if let Err(e) = grep::run(config) {
+        eprintln!("Application error: {e}");
+        process::exit(1);
     }
 }
